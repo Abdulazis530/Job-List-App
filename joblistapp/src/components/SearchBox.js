c793c69fea46c60905b5b85a7919b312acf1752d
azis <abdlazis530@gmail.com>
2020-12-23 21:09:40 +0700

    add search functionality

--- joblistapp/src/components/SearchBox.js
+++ joblistapp/src/components/SearchBox.js
@@ -1,14 +1,22 @@
-import React from 'react'
-
+import React, { useState, useEffect } from 'react'
 import searchIcon from "../assets/icons/icon-search.svg"
 
 
 export default function SearchBox() {
+    const [filter, setFilter] = useState("")
+    useEffect(() => {
+        console.log(filter)
+    }, [filter])
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFilter(value)
+    }
+
     return (
         <section className="filter__section">
 
             <div className="container-input">
-                <input name="search" placeholder="Search by job title, company or skill" className="input input--search" />
+                <input name="filter" placeholder="Search by job title, company or skill" className="input input--search" value={filter} onChange={handleChange} />
                 <img className="input-icon" src={searchIcon} alt={"icon"} />
             </div>
 
